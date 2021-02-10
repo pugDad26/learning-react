ea3bb3026e23bdb44d6600ba13a6c97c06a1d4ec
pugDad26 <63824751+pugDad26@users.noreply.github.com>
2021-02-09 19:42:59 -0600

    added styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import './App.css';
 import {Greeting} from './Greeting';
 
 const App = () => {
+
+  const styles = {
+    container: {
+      padding: 20,
+      backgroundColor: new Date().getSeconds() % 2 ? "lightblue" : "pink"
+    }
+  };
   return (
     // Looks like html but it is JSX
       // JSX Fragment, don't need a needless div
       // Elements need a parent element
-    <>
+    <div
+      style={styles.container}
+    >
       <h1>Hello React</h1>
       
       <Greeting name="Sherman" favoriteNumber={26} favoriteColors={["green", "orange", "purple"]}/>
-    </>
+    </div>
   );
 }
 
